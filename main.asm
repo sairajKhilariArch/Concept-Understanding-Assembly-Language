5504913bc1dba0585ca943c4afc871ddccffb1fc
sairajKhilariArch <sairajkhilari191918110+github1@gmail.com>
2026-04-17 13:20:12 +0530

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   main
    	modified:   main.asm
    	modified:   main.o

--- main.asm
+++ main.asm
@@ -1,10 +1,16 @@
+section .data
+    text db "Assembly is working !!!" , 50
+
 section .text
     global _start
 
 _start:
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, text
+    mov edx, 21
+    syscall
+
     mov rax, 60
-    mov rax, 40
-    mov rdi, 42
-    mov rax, 60
-    xor rdi, rdi
+    mov edi, 0
     syscall
